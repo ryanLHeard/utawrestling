eac4d1663f63ca68616c9ccdab39618e6923057d
Ryan Heard <brotherakuma@gmail.com>
2015-09-30 23:14:47 -0500

    mongodb update

--- routes/wrestlers.js
+++ routes/wrestlers.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 var db = require('mongoskin').db('mongodb://reportsUser:123456789@ec2-52-89-197-230.us-west-2.compute.amazonaws.com:27017/team');
 var wrestler = {};
+
 db.collection('wrestlers').find().toArray(function(err, result) {
   if (err) throw err;
+
   wrestler =  result;
+  //console.log(wrestler)
 })
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('wrestlers', { title: 'UTA Wrestlers', people: wrestler });
 });
-console.log(wrestler)
 module.exports = router;
