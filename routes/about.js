a81aaa76457cd5d92c70ded5809f0ed9a946bcb1
Ryan Heard <brotherakuma@gmail.com>
2015-09-30 21:59:28 -0500

    mongodb

--- routes/about.js
+++ routes/about.js
@@ -2,10 +2,11 @@
  * Created by ryan on 9/21/2015.
  */var express = require('express');
 var router = express.Router();
+;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'About UTA Wrestling' });
+  res.render('about', { title: 'About UTA Wrestling'});
 });
 
 module.exports = router;
